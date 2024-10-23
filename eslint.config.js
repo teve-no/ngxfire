fd080868266435b7214c2fbf854557563c221fa5
Atle Sjønøst <atle@teve.no>
2024-10-23 17:57:28 +0200

    chore: convert eslint config to non-compat

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,12 @@
-const { FlatCompat } = require('@eslint/eslintrc');
-const js = require('@eslint/js');
-const nxEslintPlugin = require('@nx/eslint-plugin');
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: js.configs.recommended
-});
+const nx = require('@nx/eslint-plugin');
 
 module.exports = [
-  { plugins: { '@nx': nxEslintPlugin } },
+  ...nx.configs['flat/base'],
+  ...nx.configs['flat/typescript'],
+  ...nx.configs['flat/javascript'],
+  {
+    ignores: ['**/dist']
+  },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
     rules: {
@@ -16,7 +14,7 @@ module.exports = [
         'error',
         {
           enforceBuildableLibDependency: true,
-          allow: [],
+          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
           depConstraints: [
             {
               sourceTag: '*',
@@ -27,25 +25,12 @@ module.exports = [
       ]
     }
   },
-  ...compat.config({ extends: ['plugin:@nx/typescript'] }).map(config => ({
-    ...config,
-    files: ['**/*.ts', '**/*.tsx'],
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    // Override or add rules here
+    plugins: { '@stylistic': require('@stylistic/eslint-plugin') },
     rules: {
-      ...config.rules
+      '@stylistic/no-extra-semi': 'error'
     }
-  })),
-  ...compat.config({ extends: ['plugin:@nx/javascript'] }).map(config => ({
-    ...config,
-    files: ['**/*.js', '**/*.jsx'],
-    rules: {
-      ...config.rules
-    }
-  })),
-  ...compat.config({ env: { jest: true } }).map(config => ({
-    ...config,
-    files: ['**/*.spec.ts', '**/*.spec.tsx', '**/*.spec.js', '**/*.spec.jsx'],
-    rules: {
-      ...config.rules
-    }
-  }))
+  }
 ];
