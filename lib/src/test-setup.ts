da1bd483c0e0905fee0f02ec05fc408c7f1f8213
Atle Sjønøst <atle@teve.no>
2024-11-30 03:49:35 +0100

    chore: update deps

--- lib/src/test-setup.ts
+++ lib/src/test-setup.ts
@@ -1,11 +1,2 @@
-import { getTestBed } from '@angular/core/testing';
-import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
-
-getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
-  errorOnUnknownElements: true,
-  errorOnUnknownProperties: true
-});
-
-import { TextEncoder, TextDecoder } from 'util';
-import { ReadableStream } from 'stream/web';
-Object.assign(global, { TextDecoder, TextEncoder, ReadableStream });
+import { setupZonelessTestEnv } from 'jest-preset-angular/setup-env/zoneless';
+setupZonelessTestEnv({ errorOnUnknownElements: true, errorOnUnknownProperties: true });
