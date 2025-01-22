98a56a351929dc914568e73b1e8af9d3114b84f1
Atle Sjønøst <atle@teve.no>
2025-01-22 20:23:57 +0100

    fix: block use in SSR, only support zoneless CSR

--- lib/src/app/index.ts
+++ lib/src/app/index.ts
@@ -1,7 +1,8 @@
 export * from 'firebase/app';
 
 /* eslint-disable @typescript-eslint/no-unsafe-declaration-merging */
-import { Injectable, Injector, makeEnvironmentProviders, NgZone, ɵNoopNgZone } from '@angular/core';
+import { Injectable, Injector, makeEnvironmentProviders, NgZone, PLATFORM_ID, ɵNoopNgZone } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { FirebaseApp as IFirebaseApp } from 'firebase/app';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface, @typescript-eslint/no-empty-object-type
@@ -9,11 +10,11 @@ export interface FirebaseApp extends IFirebaseApp {}
 @Injectable()
 export class FirebaseApp {}
 export const provideFirebaseApp = (fn: (injector: Injector) => IFirebaseApp) =>
-  makeEnvironmentProviders([{ provide: FirebaseApp, useFactory: validateZoneless(fn), deps: [Injector, NgZone] }]);
+  makeEnvironmentProviders([{ provide: FirebaseApp, useFactory: validateZonelessCSR(fn), deps: [Injector, NgZone, PLATFORM_ID] }]);
 
-function validateZoneless<T>(fn: (injector: Injector) => T) {
-  return (injector: Injector, zone: NgZone) => {
-    if (zone instanceof ɵNoopNgZone) return fn(injector);
-    throw Error('ngxfire supports only zoneless');
+function validateZonelessCSR<T>(fn: (injector: Injector) => T) {
+  return (injector: Injector, zone: NgZone, platformId: object) => {
+    if (zone instanceof ɵNoopNgZone && isPlatformBrowser(platformId)) return fn(injector);
+    throw Error('@teve/ngxfire only supports zoneless CSR, use @angular/fire instead');
   }
 }
