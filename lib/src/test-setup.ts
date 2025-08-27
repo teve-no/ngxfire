6aae7eb62b16a054b3f0861cb739d96a429eeb71
Atle Sjønøst <atle@teve.no>
2025-08-27 13:12:07 +0200

    chore: update deps

--- lib/src/test-setup.ts
+++ lib/src/test-setup.ts
@@ -1,3 +1,4 @@
+import '@angular/compiler';
 import '@analogjs/vitest-angular/setup-snapshots';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
