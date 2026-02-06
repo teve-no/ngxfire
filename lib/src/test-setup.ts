84dd600da335de346bb20778ae06f824f5ede10b
Atle Sjønøst <atle@teve.no>
2026-02-06 12:58:36 +0100

    chore: remove vitest

--- lib/src/test-setup.ts
+++
@@ -1,8 +0,0 @@
-import '@angular/compiler';
-import '@analogjs/vitest-angular/setup-snapshots';
-import { getTestBed } from '@angular/core/testing';
-import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
-getTestBed().initTestEnvironment(BrowserTestingModule, platformBrowserTesting(), {
-  errorOnUnknownElements: true,
-  errorOnUnknownProperties: true
-});
